f5b9b3c41f5d9985400aac1f611a13fc5f434dd6
godshen <shenzh0713@gmail.com>
2016-02-22 13:27:26 +0800

    v0.08

--- init.lua
+++ init.lua
@@ -54,133 +54,6 @@ function MoveBit( dir , data , num )
 end
 
 
-function BitOperationAND( a , b )
-  local res = 0
-
-  for i=8,1,-1
-    do
-    res = MoveBit('L',res,1)
-
-    t1 = MoveBit('R',a,i-1)
-    t2 = MoveBit('R',a,i)
-    t2 = MoveBit('L',t2,1)
-    aa = t1 - t2    
-
-    t1 = MoveBit('R',b,i-1)
-    t2 = MoveBit('R',b,i)
-    t2 = MoveBit('L',t2,1)
-    bb = t1 - t2    
-
-    if( ( aa==1 ) and ( bb==1) )then
-      res = res + 1
-      --print(res)
-      --print(aa..'&'..bb..'->'..'1')
-    else
-      --print(aa..'&'..bb..'->'..'0')
-    end
-  end
-  --print("end of and")
-  return res
-end
-
-
-function BitOperationOR( a , b )
-  local res = 0
-
-
-  for i=8,1,-1
-    do
-    res = MoveBit('L',res,1)
-
-    t1 = MoveBit('R',a,i-1)
-    t2 = MoveBit('R',a,i)
-    t2 = MoveBit('L',t2,1)
-    aa = t1 - t2    
-
-    t1 = MoveBit('R',b,i-1)
-    t2 = MoveBit('R',b,i)
-    t2 = MoveBit('L',t2,1)
-    bb = t1 - t2    
-
-    if( ( aa==0 ) and ( bb==0) )then
-      res = res + 0
-      --print(res)
-      --print(aa..'&'..bb..'->'..'0')
-    else
-      res = res + 1
-      --print(aa..'&'..bb..'->'..'1')
-    end
-  end
-  --print("end of and")
-  return res
-end
-
-
-function BitOperationXOR( a , b )
-  local mask
-  local res = 0
-  local aa
-  local bb
-  
-  for i = 7 , 0 , -1
-    do
-    res = MoveBit('L',res,1)
-    mask = MoveBit('L',1,i)
-    aa = BitOperationAND(a,mask)
-    bb = BitOperationAND(b,mask)
-
-    if aa == bb then
-      res = res + 0
-    else
-      res = res + 1
-    end
-
-  end
-  return res
-end
-
-
-function CRC16( pdata, datalen)
-
-  local CRC16Lo,CRC16Hi,CL,CH,SaveHi,SaveLo;
-  local i,Flag;
-  
-  CRC16Lo = 0xFF;
-  CRC16Hi = 0xFF;
-  CL = 0x01;
-  CH = 0xA0;
-
-  for i=1,datalen,1
-    do
-    CRC16Lo = BitOperationXOR(CRC16Lo , pdata[i]);
-    
-    for Flag=0,7,1
-      do
-      SaveHi = CRC16Hi;
-      SaveLo = CRC16Lo;
-      CRC16Hi = MoveBit('R',CRC16Hi,1)
-      CRC16Lo = MoveBit('R',CRC16Lo,1)
-      
-      if(BitOperationAND(SaveHi , 0x01) == 0x01) then
-      --if((SaveHi & 0x01) == 0x01) then
-        --print(SaveHi)
-        CRC16Lo = BitOperationOR( CRC16Lo , 0x80 );
-      end
-
-      if(BitOperationAND(SaveLo , 0x01) == 0x01) then
-      --if((SaveLo & 0x01) == 0x01) then
-        --print(SaveLo)
-        CRC16Hi = BitOperationXOR(CRC16Hi , CH);
-        CRC16Lo = BitOperationXOR(CRC16Lo , CL);
-      end
-
-    end 
-  end 
-
-  return  BitOperationOR(MoveBit('L',CRC16Hi,8) , CRC16Lo)
-end
-
-
 
 function getnumber( index )
    return strTonum(string.byte(string.sub(strload,index,index)))
@@ -229,15 +102,9 @@ function _M.decode(payload)
     strload = payload;
     packet['status'] = 'not'
 
-    DATALENGTH = getnumber(3) * 256 + getnumber(4);
-    local crcdata = {}
-    for i=1,DATALENGTH,1
-      do
-      crcdata[i] = getnumber(i)
-    end
-    
-    --if CRC16( crcdata , DATALENGTH + 4 ) == (getnumber(43)*256+getnumber(44) ) then
-    if( true ) then
+    --DATALENGTH = getnumber(3) * 256 + getnumber(4); 
+
+    if( false ) then
       local head1 = string.sub(payload,1,1)
       local head2 = string.sub(payload,2,2)
 
@@ -268,7 +135,8 @@ function _M.decode(payload)
       packet['status'] = 'CRC-ERROR'
     end
 
-    return Json(packet)
+    bittest['test1']='ok'
+    return Json(bittest)
 end
 
 return _M
